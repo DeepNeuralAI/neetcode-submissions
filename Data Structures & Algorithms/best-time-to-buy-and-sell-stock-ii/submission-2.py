b7b3051ac0ce425819777e5955efe336288808b0
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-06-26 12:01:06 -0400

    Add: best-time-to-buy-and-sell-stock-ii - submission-2

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        dp = [[0] * 2 for _ in range(len(prices))]
+        dp[0][1] = -prices[0]
+
+
+        for i in range(1, n):
+            for j in range(2):
+                if j == 0:
+                    dp[i][j] = max(
+                        prices[i] + dp[i - 1][1 - j],
+                        dp[i - 1][j]
+                    )
+                elif j == 1:
+                    dp[i][j] = max(
+                        -prices[i] + dp[i - 1][1 - j],
+                        dp[i - 1][j]
+                    )
+        return max(dp[n - 1])
+
+
+
+        
