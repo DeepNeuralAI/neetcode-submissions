5d6bedfe67936822e89fe1c3cd61e1d132a14b83
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-06-27 11:00:58 -0400

    Add: valid-palindrome-ii - submission-4

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-4.py
@@ -0,0 +1,31 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+       n = len(s)
+       l = 0
+       r = n - 1
+       
+       while l < r:
+            if s[l] != s[r]:
+                skipL = s[l + 1: r + 1]
+                skipR = s[l : r]
+
+                return (self.isPalindrome(skipL) or 
+                        self.isPalindrome(skipR))
+            
+            l += 1
+            r -= 1
+        
+       return True
+
+    def isPalindrome(self, s):
+        n = len(s)
+        i = 0
+        j = n - 1
+
+        while i < j:
+            if s[i] != s[j]:
+                return False
+            i += 1
+            j -= 1
+        return True
+            
