50e05e5c062f69be489c3acddb990e25647625ad
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-06-27 16:48:01 -0400

    Add: three-integer-sum - submission-5

---
+++ Data Structures & Algorithms/three-integer-sum/submission-5.py
@@ -0,0 +1,39 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # Sorted - Less Space
+        nums.sort()
+        n = len(nums)
+        ans = []
+
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            
+            j = i + 1
+            k = n - 1
+
+            while j < k:
+
+                candidate = nums[i] + nums[j] + nums[k]
+                
+                if candidate == 0:
+                    ans.append([nums[i], nums[j], nums[k]])
+                    j += 1
+                    k -= 1
+            
+                    while j < k and nums[j] == nums[j - 1]:
+                        j += 1
+                
+                    while j < k and nums[k] == nums[k + 1]:
+                        k -= 1
+                elif candidate < 0:
+                    j += 1
+                else:
+                    k -= 1
+        return ans
+
+
+
+
+
+        
