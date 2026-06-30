30057469f913675c264bdabf2c7cfe7e1dc65ef1
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-06-30 09:34:46 -0400

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        n = len(s)
+        l = 0
+        r = 0
+        res = 0
+
+        count = defaultdict(int)
+        maxFreq = 0
+
+        while r < n:
+            count[s[r]] += 1
+            maxFreq = max(maxFreq, count[s[r]])
+
+            while (r - l + 1) - maxFreq > k:
+                count[s[l]] -= 1
+                l += 1
+            
+            res = max(res, r - l + 1)
+            
+            r += 1
+        return res
+
+
+
+
+
+        
