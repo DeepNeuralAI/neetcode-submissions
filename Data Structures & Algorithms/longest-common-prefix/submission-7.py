e007c36db32ee3548737678ab4bd1d6bb49c0bbd
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-07-03 16:35:06 -0400

    Add: longest-common-prefix - submission-7

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-7.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        n = len(strs)
+        strs.sort()
+        res = ""
+        
+        s1, s2 = strs[0], strs[n - 1]
+        for i in range(len(s1)):
+            if i >= len(s2) or s2[i] != s1[i]:
+                return res
+            res += s1[i]
+        
+        return res
+        
