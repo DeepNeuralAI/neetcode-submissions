00702e7e47513e8d742cc69f3f4ad010e583993b
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-07-04 16:58:43 -0400

    Add: search-in-rotated-sorted-array-ii - submission-7

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-7.py
@@ -0,0 +1,27 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+        
+            if nums[m] == target:
+                return True
+            
+            if nums[m] == nums[r] == nums[l]:
+                l += 1
+                r -= 1
+                continue
+
+            # Right Sorted Section
+            if nums[m] <= nums[r]:
+                if nums[r] >= target > nums[m]:
+                    l = m + 1
+                else:
+                    r = m - 1
+            else:
+                if nums[l] <= target < nums[m]:
+                    r = m - 1
+                else:
+                    l = m + 1
+        return False
+   
