30a5f586e26665015af2b93da4f38c47fb846f81
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-07-04 13:20:16 -0400

    Add: search-2d-matrix - submission-4

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-4.py
@@ -0,0 +1,16 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # Staircase Method
+        ROWS, COLS = len(matrix), len(matrix[0])
+        r = 0
+        c = COLS - 1
+
+        while r < ROWS and c >= 0:
+            if matrix[r][c] == target:
+                return True
+            
+            if matrix[r][c] < target:
+                r += 1
+            else:
+                c -= 1
+        return False
