6c703d8108b27ee8b5f649e54e5eb969d3d73027
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-07-05 13:40:28 -0400

    Add: find-in-mountain-array - submission-3

---
+++ Data Structures & Algorithms/find-in-mountain-array/submission-3.py
@@ -0,0 +1,46 @@
+class Solution:
+    def findInMountainArray(self, target: int, mountainArr: 'MountainArray') -> int:
+        length = mountainArr.length()
+
+        # Find Peak
+        l, r = 1, length - 2
+
+        while l <= r:
+            m = (l + r) // 2
+
+            left, mid, right = mountainArr.get(m - 1), mountainArr.get(m), mountainArr.get(m + 1)
+            if left < mid < right:
+                l = m + 1
+            elif left > mid > right:
+                r = m - 1
+            else:
+                break
+
+        peak = m
+
+        l, r = 0, peak
+        # Left Section
+        while l <= r:
+            m = (l + r) // 2
+            val = mountainArr.get(m)
+            if val  == target:
+                return m
+            if val < target:
+                l = m + 1
+            else:
+                r = m - 1
+        
+
+        l, r = peak, length - 1
+        # Right Section
+        
+        while l <= r:
+            m = (l + r) // 2
+            val = mountainArr.get(m)
+            if val  == target:
+                return m
+            if val > target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
