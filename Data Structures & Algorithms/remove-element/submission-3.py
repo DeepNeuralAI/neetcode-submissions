16abf13ed693ebafda79ca4e89ec7c74ac07aba1
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-07-07 09:40:00 -0400

    Add: remove-element - submission-3

---
+++ Data Structures & Algorithms/remove-element/submission-3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        last = 0
+        n = len(nums)
+
+        for i in range(n):
+            if nums[i] != val:
+                nums[last] = nums[i]
+                last += 1
+        return last
+        
