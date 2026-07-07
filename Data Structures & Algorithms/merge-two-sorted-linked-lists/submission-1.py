b46877ae0061be869f1a4fc89320f14de95a195b
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-07-07 11:07:04 -0400

    Add: merge-two-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-1.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(-1)
+
+        curr = dummy
+        h1, h2 = list1, list2
+
+        while h1 and h2:
+            if h1.val <= h2.val:
+                curr.next = ListNode(h1.val)
+                h1 = h1.next
+            else:
+                curr.next = ListNode(h2.val)
+                h2 = h2.next
+            
+            curr = curr.next
+        
+        if h1:
+            curr.next = h1
+        
+        if h2:
+            curr.next = h2
+        
+    
+        return dummy.next
+            
