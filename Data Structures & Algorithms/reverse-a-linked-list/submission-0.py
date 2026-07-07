36c88891c01c24ae8a37946fab75961b6c471428
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-07-07 10:24:20 -0400

    Add: reverse-a-linked-list - submission-0

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-0.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None or head.next is None:
+            return head
+        
+        prev = None
+        curr = head
+
+        # 1 -> 2 -> None
+
+        while curr:
+            next = curr.next
+            curr.next = prev
+
+            prev = curr
+            curr = next
+                
+        return prev
