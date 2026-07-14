bf6f4de328d756e14099d207688e2821d5773eac
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-07-14 14:12:42 -0400

    Add: rotate-array - submission-5

---
+++ Data Structures & Algorithms/rotate-array/submission-5.py
@@ -0,0 +1,21 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k = k % n
+
+        # Optimal
+        self.reverse(nums, 0, n - k - 1)
+        self.reverse(nums, n - k, n - 1)
+        self.reverse(nums, 0, n - 1)
+        return nums
+
+    
+
+    def reverse(self, nums, i, j):
+        while i < j:
+            nums[i], nums[j] = nums[j], nums[i]
+            i += 1
+            j -= 1
