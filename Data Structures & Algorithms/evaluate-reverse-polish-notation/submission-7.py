1067ef264d4fd2dbfe65ae38fcd822d113061d15
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-07-15 13:47:56 -0400

    Add: evaluate-reverse-polish-notation - submission-7

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-7.py
@@ -0,0 +1,33 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for op in tokens:
+            if op == '+':
+                second = stack.pop()
+                first = stack.pop()
+
+                stack.append(first + second)
+            elif op == '*':
+                first = stack.pop()
+                second = stack.pop()
+
+                stack.append(first * second)
+            elif op == '/':
+                divisor = stack.pop()
+                dividend = stack.pop()
+
+                stack.append(int(dividend / divisor))
+            elif op == '-':
+                first = stack.pop()
+                second = stack.pop()
+
+                stack.append(second - first)
+            else:
+                stack.append(int(op))
+        print(stack)
+        return stack[-1]
+
+
+
+
+        
