b884c6c6205b749c9928ed109cc380dce5af94e6
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-07-15 10:16:48 -0400

    Add: minimum-size-subarray-sum - submission-4

---
+++ Data Structures & Algorithms/minimum-size-subarray-sum/submission-4.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        curSum = 0
+        n = len(nums)
+        minLen = len(nums) + 1
+
+        l = r = 0
+
+        while r < n:
+            curSum += nums[r]
+
+            while curSum >= target:
+                minLen = min(r - l + 1, minLen)
+                curSum -= nums[l]
+                l += 1
+            
+            r += 1
+        
+        return 0 if minLen > n else minLen
