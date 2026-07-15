e2221b07a541808c6dc7f8083971943d746439ed
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-07-15 14:53:50 -0400

    Add: asteroid-collision - submission-12

---
+++ Data Structures & Algorithms/asteroid-collision/submission-12.py
@@ -0,0 +1,21 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        
+
+        for a in asteroids:
+            if a > 0:
+                stack.append(a)
+                continue
+
+            while stack and stack[-1] > 0 and stack[-1] < abs(a):
+                stack.pop()
+            
+            if stack and stack[-1] == abs(a):
+                stack.pop()
+            elif stack and stack[-1] > 0:
+                continue
+            else:
+                stack.append(a)
+        
+        return stack
