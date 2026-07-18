4f022c7407d01b621f18a3809ae0c5b06b506445
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-07-18 13:22:29 -0400

    Add: permutation-string - submission-4

---
+++ Data Structures & Algorithms/permutation-string/submission-4.py
@@ -0,0 +1,32 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s2) < len(s1):
+            return False
+        
+        # Brute Force
+        n = len(s1)
+        m = len(s2)
+        count1 = defaultdict(int)
+
+        for c in s1:
+            count1[c] += 1
+        
+        require = len(count1)
+        
+        for i in range(m - n + 1):
+            window = defaultdict(int)
+            met = 0
+            for j in range(i, i + n):
+                char = s2[j]
+                if char in count1:
+                    window[char] += 1
+
+                    if window[char] == count1[char]:
+                        met += 1
+
+                    if met == require:
+                        return True
+                else:
+                    break
+        return False
+        
