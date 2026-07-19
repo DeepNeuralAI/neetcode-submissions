f4264fcc84cc719042d1aa508b497f3404197239
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-07-19 12:09:47 -0400

    Add: balanced-binary-tree - submission-0

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-0.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        if root is None:
+            return True
+        
+        res = self.height(root)
+
+        if res is False:
+            return False
+        
+        return True
+        
+       
+    def height(self, node):
+        if node is None:
+            return 0
+        
+        left = self.height(node.left)
+        right = self.height(node.right)
+
+        if left is False or right is False:
+            return False
+        
+        if abs(left - right) > 1:
+            return False
+
+        return 1 + max(left, right)
