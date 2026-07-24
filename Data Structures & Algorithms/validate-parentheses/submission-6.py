ac53ca3353145719305205330e61abdb93e87749
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-07-24 14:07:59 -0400

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if not s:
+            return True
+        
+        mapping = {
+            ']': '[',
+            '}': '{',
+            ')': '('
+        }
+
+        stack = []
+
+        for bracket in s:
+            if bracket in mapping:
+                if not stack or stack[-1] != mapping[bracket]: return False
+                stack.pop()
+            else:
+                stack.append(bracket)
+    
+        return not stack
+        
