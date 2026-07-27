6ec8f1b4be7b00a41c27e2bf673c0a145966b1f7
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-07-27 15:51:06 -0400

    Add: find-k-closest-elements - submission-7

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-7.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        n = len(arr)
+        
+        window_diff = 0
+        for i in range(k):
+            window_diff += abs(arr[i] - x)
+        
+        min_diff = window_diff
+        ansStart = 0
+        ansEnd = k - 1
+        
+        l = 0
+        r = k
+
+        while r < n:
+            window_diff += abs(arr[r] - x)
+            window_diff -= abs(arr[l] - x)
+            l += 1
+
+            if window_diff < min_diff:
+                ansStart = l
+                ansEnd = r
+                min_diff = window_diff
+
+            r += 1
+        
+        return arr[ansStart : ansEnd + 1]
+
