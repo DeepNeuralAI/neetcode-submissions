a1a93b69a55f00e22e141d90a61b8f93c73733ad
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-07-29 13:05:50 -0400

    Add: single-threaded-cpu - submission-1

---
+++ Data Structures & Algorithms/single-threaded-cpu/submission-1.py
@@ -0,0 +1,29 @@
+class Solution:
+    def getOrder(self, tasks: List[List[int]]) -> List[int]:
+        for i, t in enumerate(tasks):
+            t.append(i)
+        
+        tasks.sort(key = lambda t: t[0])
+        minHeap, res = [], [] # (processing_time, task_id)
+        i = time = 0
+
+        while i < len(tasks) or minHeap:
+            while i < len(tasks) and tasks[i][0] <= time:
+                heapq.heappush(minHeap, (tasks[i][1], tasks[i][2]))
+                i += 1
+           
+            if minHeap:
+                proc_time, task_id = heapq.heappop(minHeap)
+                time += proc_time
+                res.append(task_id)
+            
+            if not minHeap and i < len(tasks):
+                time = max(time, tasks[i][0])
+        
+
+        return res
+
+            
+            
+
+        
