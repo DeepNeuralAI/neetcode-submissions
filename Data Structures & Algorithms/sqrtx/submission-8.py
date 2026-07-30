bc20a232a46e1e81c3d1d5affb83622ff6cb962b
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-07-30 13:09:13 -0400

    Add: sqrtx - submission-8

---
+++ Data Structures & Algorithms/sqrtx/submission-8.py
@@ -0,0 +1,25 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+
+        l = 1
+        r = x
+
+        res = 1
+        while l <= r:
+            m = (l + r) // 2
+            candidate = m * m
+
+            if candidate == x:
+                return m
+            
+            if candidate < x:
+                res = m
+                l = m + 1
+            else:
+                r = m - 1
+        
+        return res
+
+        
