af0be06b236785b0581cd8353d938d45a3372d8c
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-08-04 13:50:13 -0400

    Add: palindrome-partitioning - submission-0

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-0.py
@@ -0,0 +1,28 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        res = []
+        self.solve(s, 0, [], res)
+        return res
+        
+    def solve(self, s, start, path, res):
+        if start == len(s):
+            res.append(path.copy())
+            return
+        
+        for end in range(start, len(s)):
+            if self.isPalindrome(s, start, end):
+                path.append(s[start: end + 1])
+                self.solve(s, end + 1, path, res)
+                path.pop()
+
+            
+        
+
+    def isPalindrome(self, s, i, j):
+        while i < j:
+            if s[i] != s[j]:
+                return False
+            i += 1
+            j -= 1
+        return True
+        
