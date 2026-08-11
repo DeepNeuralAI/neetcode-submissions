fa670f78dc8de0b47e983342b55ec0682e9452d1
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-08-11 14:23:07 -0400

    Add: contains-duplicate-ii - submission-11

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-11.py
@@ -0,0 +1,23 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        l = r = 0
+        n = len(nums)
+        seen = set()
+
+        while r < n:
+            if r - l > k:
+                seen.remove(nums[l])
+                l += 1
+
+            if nums[r] in seen:
+                return True
+
+            seen.add(nums[r])
+            r += 1
+        
+        return False
+
+
+
+
+        
