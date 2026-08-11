d739cebb2bd1faa15e0c6922925ff292874611bf
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-08-11 14:34:02 -0400

    Add: car-fleet - submission-8

---
+++ Data Structures & Algorithms/car-fleet/submission-8.py
@@ -0,0 +1,16 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pairs = [(p, s) for p, s in zip(position, speed)]
+        pairs.sort(reverse = True)
+        
+        stack = []
+
+        for p, s in pairs:
+            time_to_dest = (target - p) / s
+
+            stack.append(time_to_dest)
+            
+            if len(stack) > 1 and stack[-1] <= stack[-2]:
+                stack.pop()
+            
+        return len(stack)
