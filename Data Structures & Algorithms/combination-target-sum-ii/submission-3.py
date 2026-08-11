4ba980676e847f0c9fc7be8dda506cd85c077132
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-08-11 11:26:54 -0400

    Add: combination-target-sum-ii - submission-3

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        if not candidates:
+            return []
+
+        candidates.sort()
+        current, res = [], []
+
+        def backtrack(start, target):
+            if target == 0:
+                res.append(current.copy())
+                return
+            
+            for i in range(start, len(candidates)):
+                if i > start and candidates[i - 1] == candidates[i]:
+                    continue
+
+                if target >= candidates[i]:
+                    current.append(candidates[i])
+                    backtrack(i + 1, target - candidates[i])
+                    current.pop()
+        
+        backtrack(0, target)
+        return res
