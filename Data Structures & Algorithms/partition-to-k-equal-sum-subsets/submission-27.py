6268ea0d56314ffefb4eeed75abecd83a9b98404
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-08-12 14:16:50 -0400

    Add: partition-to-k-equal-sum-subsets - submission-27

---
+++ Data Structures & Algorithms/partition-to-k-equal-sum-subsets/submission-27.py
@@ -0,0 +1,40 @@
+class Solution:
+    def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
+        total = sum(nums)
+        if total % k != 0:
+            return False
+        
+        nums.sort(reverse = True)
+        subset_target = total // k
+        used = [False] * len(nums)
+
+       
+        def backtrack(start, target, k):
+            if k == 0:
+                return True
+            
+            if target == 0:
+                return backtrack(0, subset_target, k - 1)
+        
+            
+            for i in range(start, len(nums)):
+                if used[i]: continue
+                
+                if (i > start) and nums[i] == nums[i - 1] and not used[i -1]:
+                    continue
+                
+                if target - nums[i] >= 0:
+                    used[i] = True
+                    if backtrack(i + 1, target - nums[i], k):
+                        return True
+                    used[i] = False
+            return False
+        
+        return backtrack(0, subset_target, k)
+            
+
+        
+                    
+
+
+        
