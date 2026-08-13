a195bc12978e8d405a41fa969cc30b10e4c5a026
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-08-13 14:49:45 -0400

    Add: n-queens-ii - submission-3

---
+++ Data Structures & Algorithms/n-queens-ii/submission-3.py
@@ -0,0 +1,43 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        cols = set()
+        pos_diagonals = set()
+        neg_diagonals = set()
+
+        res = []
+    
+        def backtrack(row):
+            if row == n:
+                return 1
+            
+            num_ways = 0
+            for col in range(n):
+                if isSafe(row, col):
+                    cols.add(col)
+                    pos_diagonals.add(row + col)
+                    neg_diagonals.add(row - col)
+                    
+                    num_ways += backtrack(row + 1)
+
+                    cols.remove(col)
+                    pos_diagonals.remove(row + col)
+                    neg_diagonals.remove(row - col)
+            
+            return num_ways
+            
+
+        def isSafe(r, c):
+            if c in cols:
+                return False
+            
+            if (r + c) in pos_diagonals:
+                return False
+            
+            if (r - c) in neg_diagonals:
+                return False
+            
+            return True
+        
+        return backtrack(0)
+        
+ 
