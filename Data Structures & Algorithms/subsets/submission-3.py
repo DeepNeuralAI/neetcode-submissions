03cab4999ee1c34c44a7e2c2a2c7c9deb19ff831
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-08-18 11:52:47 -0400

    Add: subsets - submission-3

---
+++ Data Structures & Algorithms/subsets/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        curr, res = [], []
+        
+        def backtrack(start):
+            res.append(curr.copy())
+
+            for i in range(start, len(nums)):
+                curr.append(nums[i])
+                backtrack(i + 1)
+                curr.pop()
+        
+        backtrack(0)
+        return res
+        
