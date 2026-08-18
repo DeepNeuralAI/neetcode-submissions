deead7e4511e15bedd518cdf35e18ffdc7b88329
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-08-18 12:09:40 -0400

    Add: combinations - submission-5

---
+++ Data Structures & Algorithms/combinations/submission-5.py
@@ -0,0 +1,18 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        curr, res = [], []
+        
+        def backtrack(start):
+            if len(curr) == k:
+                res.append(curr.copy())
+                return 
+
+            for i in range(start, n + 1):
+                curr.append(i)
+                backtrack(i + 1)
+                curr.pop()
+        
+        backtrack(1)
+        return res
+
+        
