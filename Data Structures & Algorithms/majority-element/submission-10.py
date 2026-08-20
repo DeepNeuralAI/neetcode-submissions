8762d81998ea6fbf6762684c821c07c926aaf9a5
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-08-19 22:50:31 -0400

    Add: majority-element - submission-10

---
+++ Data Structures & Algorithms/majority-element/submission-10.py
@@ -0,0 +1,23 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        n = len(nums)
+        count_needed = n / 2
+
+        cnt = 0
+        candidate = None
+
+        for i in range(n):
+            if cnt == 0:
+                candidate = nums[i]
+                cnt += 1
+            elif nums[i] == candidate:
+                cnt += 1
+            else:
+                cnt -= 1
+        
+        if nums.count(candidate) > count_needed:
+            return candidate
+        
+        return None
+
+        
