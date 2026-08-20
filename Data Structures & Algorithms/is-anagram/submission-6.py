b7e9ff7f5effafe8bf23537ae778b6635da806d0
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-08-19 22:18:13 -0400

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        n = len(s)
+        m = len(t)
+
+        if n != m:
+            return False
+        
+        countS = defaultdict(int)
+        countT = defaultdict(int)
+
+        for i in range(n):
+            countS[s[i]] += 1
+            countT[t[i]] += 1
+        
+        for c in countS:
+            if countS[c] != countT.get(c, 0):
+                return False
+        
+        return True
+        
