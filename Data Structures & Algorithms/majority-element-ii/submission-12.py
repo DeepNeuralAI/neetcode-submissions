e924bd695e334849cfea6ee4a9620dd34e09f9e4
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-08-22 16:56:34 -0400

    Add: majority-element-ii - submission-12

---
+++ Data Structures & Algorithms/majority-element-ii/submission-12.py
@@ -0,0 +1,26 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        # Maintaining a candidate dict of maximum of size 2
+        count = defaultdict(int)
+
+        for num in nums:
+            count[num] += 1
+
+            if len(count) > 2:
+                count_copy = defaultdict(int)
+                
+                for candidate in count:
+                    count[candidate] -= 1
+                
+                    if count[candidate] > 0:
+                        count_copy[candidate] = count[candidate]
+                
+                count = count_copy
+
+        count_required = len(nums) / 3
+        res = []
+        for candidate in count:
+            if nums.count(candidate) > count_required:
+                res.append(candidate)
+
+        return res        
