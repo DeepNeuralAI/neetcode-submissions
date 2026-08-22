cc252d4c4b9b2d62fc43d373eb801c1490fa93b5
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-08-22 16:10:11 -0400

    Add: merge-strings-alternately - submission-7

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-7.py
@@ -0,0 +1,20 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        i = j = 0
+        n = len(word1)
+        m = len(word2)
+        res = []
+
+        while i < n and j < m:
+            res.append(word1[i])
+            res.append(word2[j])
+            i += 1
+            j += 1
+        
+        if i < n:
+            res.append(word1[i:])
+        
+        if j < m:
+            res.append(word2[j:])
+        
+        return ''.join(res)
