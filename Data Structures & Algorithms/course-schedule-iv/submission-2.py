401cdb63469b3f479234b828bed3af77adb3010d
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-08-25 21:51:15 -0400

    Add: course-schedule-iv - submission-2

---
+++ Data Structures & Algorithms/course-schedule-iv/submission-2.py
@@ -0,0 +1,31 @@
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        reachable = defaultdict(set)
+
+        graph = self.getAdjList(prerequisites)
+        
+        def dfs(node, u):
+            if node not in reachable[u]:
+                reachable[u].add(node)
+
+                for adj in graph[node]:
+                    dfs(adj, node)
+            
+                reachable[u].update(reachable[node])
+            return
+
+        for crs in range(numCourses):
+            if crs not in reachable:
+                dfs(crs, crs)
+        
+        res = []
+        for u, v in queries:
+            res.append(v in reachable[u])
+        return res
+
+
+    def getAdjList(self, edges):
+        graph = defaultdict(list)
+        for src, dst in edges:
+            graph[src].append(dst)
+        return graph    
