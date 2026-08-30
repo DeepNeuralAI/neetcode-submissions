5dd3df264622bc222f938768d11e58c111391c52
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-08-30 19:57:55 -0400

    Add: permutations - submission-5

---
+++ Data Structures & Algorithms/permutations/submission-5.py
@@ -0,0 +1,24 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        used = [False] * len(nums)
+        res = []
+
+        def backtrack(curr):
+            if len(curr) == len(nums):
+                res.append(curr.copy())
+                return
+            
+            for i in range(len(nums)):
+                if used[i]:
+                    continue
+                
+                used[i] = True
+                curr.append(nums[i])
+                backtrack(curr)
+
+                used[i] = False
+                curr.pop()
+        
+        backtrack([])
+        return res
+        
