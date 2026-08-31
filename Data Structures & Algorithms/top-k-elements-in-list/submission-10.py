2948c3cc17b63153f61b64c3a69d3f631c9fc4ee
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-08-30 20:06:42 -0400

    Add: top-k-elements-in-list - submission-10

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-10.py
@@ -0,0 +1,30 @@
+import heapq
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # Min Heap with (cnt, num) as key
+        min_heap = []
+        count = defaultdict(int)
+
+        for num in nums:
+            count[num] += 1
+        
+        pairs = []
+        
+        for num, freq in count.items():
+            if len(min_heap) < k:
+                heapq.heappush(min_heap, (freq, num))
+            else:
+                if freq > min_heap[0][0]:
+                    heapq.heappop(min_heap)
+                    heapq.heappush(min_heap, (freq, num))
+        
+        res = []
+        while min_heap:
+            freq, num = heapq.heappop(min_heap)
+            res.append(num)
+        
+        return res
+
+
+
