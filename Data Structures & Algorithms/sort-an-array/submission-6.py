5493c629ea681eb2e0b3311af2d7c9ea0fd0cd45
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-09-02 20:17:27 -0400

    Add: sort-an-array - submission-6

---
+++ Data Structures & Algorithms/sort-an-array/submission-6.py
@@ -0,0 +1,22 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        nums = self.bubble(nums)
+        return nums
+
+
+
+    def bubble(self, nums):
+        n = len(nums)
+
+        for i in range(n - 1):
+            swapped = False
+            for j in range(n - i - 1):
+                if nums[j] > nums[j + 1]:
+                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
+                    swapped = True
+            
+            if not swapped:
+                break
+        
+            
+        return nums
