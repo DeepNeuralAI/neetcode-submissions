a2498f614435cb37af0c7b4bde2d5c4924142746
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-09-03 22:09:53 -0400

    Add: remove-node-from-end-of-linked-list - submission-12

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-12.py
@@ -0,0 +1,44 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+   
+        if head is None or (n == 1 and head.next is None):
+            return None
+        
+        # Recursive Solution
+        def remove(node, n):
+            if node is None:
+                return 0
+            
+            index = remove(node.next, n) + 1
+
+            if index == (n + 1):
+                node.next = node.next.next
+            
+            return index
+        
+        dummy = ListNode(0, head)
+        curr = dummy
+        remove(curr, n)
+        return dummy.next
+        
+            
+
+            
+
+
+
+        
+
+        
+
+        
+
+        
+
+        
