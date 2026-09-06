54c1bdcf8d320d6f4aebd1d9783de504d941b47a
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-09-05 23:27:56 -0400

    Add: rotate-array - submission-19

---
+++ Data Structures & Algorithms/rotate-array/submission-19.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        temp = []
+        n = len(nums)
+        k = k % n
+        
+        for i in range(n - k, n):
+            temp.append(nums[i])
+        
+        for i in range(n - k - 1, -1, -1):
+            nums[i + k] = nums[i]
+        
+        for i in range(k):
+            nums[i] = temp[i]
