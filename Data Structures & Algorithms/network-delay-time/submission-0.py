d613b831fb0bb32508ffda9113f0d707bc9ce56b
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-09-08 23:17:43 -0400

    Add: network-delay-time - submission-0

---
+++ Data Structures & Algorithms/network-delay-time/submission-0.py
@@ -0,0 +1,46 @@
+import heapq as pq
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        graph = self.buildAdjList(times)
+
+        def bfs():
+            heap = []
+            cost = [float('inf')] * (n + 1)
+            cost[k] = 0
+            pq.heappush(heap, (0, k))
+            visited = set()
+
+            while heap:
+                t, node = pq.heappop(heap)
+                visited.add(node)
+                
+                for adj, wt in graph[node]:
+                    if adj not in visited:
+                        if cost[adj] > wt + t:
+                            cost[adj] = wt + t
+                            pq.heappush(heap, (cost[adj], adj))
+        
+            return cost
+
+        dist = bfs()
+        res = 0
+        for node in range(1, n + 1):
+            if dist[node] == float('inf'):
+                return -1
+            res = max(res, dist[node])
+        
+        return res
+            
+
+                    
+                        
+
+
+
+    def buildAdjList(self, edges):
+        adjList = defaultdict(list)
+
+        for u, v, time in edges:
+            adjList[u].append((v, time))
+        return adjList
