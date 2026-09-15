7038a1bd95bb73caa907daaa577435c2b601c331
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-09-15 11:03:24 -0400

    Add: pacific-atlantic-water-flow - submission-3

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-3.py
@@ -0,0 +1,45 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        ROWS, COLS = len(heights), len(heights[0])
+        
+        def bfs(q, visit):
+
+            while q:
+                r, c = q.popleft()
+
+                for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                    r_ = r + dr
+                    c_ = c + dc
+
+                    if not (0 <= r_ < ROWS and 0 <= c_ < COLS) or (r_, c_) in visit:
+                        continue
+                    
+                    if heights[r_][c_] >= heights[r][c]:
+                        visit.add((r_, c_))
+                        q.append((r_, c_))
+            
+        pacific, atlantic = set(), set()
+        for r in range(ROWS):
+            pacific.add((r, 0))
+            atlantic.add((r, COLS - 1))
+        
+        for c in range(COLS):
+            pacific.add((0, c))
+            atlantic.add((ROWS - 1, c))
+
+        pac = deque(pacific)
+        atl = deque(atlantic) 
+        
+        bfs(pac, pacific)
+        bfs(atl, atlantic)
+
+        res = []
+        for r in range(ROWS):
+            for c in range(COLS):
+                if (r, c) in pacific and (r, c) in atlantic:
+                    res.append([r, c])
+        return res
+
+
+
+        
