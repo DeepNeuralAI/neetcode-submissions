122f79762581c55078097601fb881f424a697ecd
Aaron M <34294344+DeepNeuralAI@users.noreply.github.com>
2026-09-22 23:56:00 -0400

    Add: top-k-elements-in-list - submission-13

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-13.py
@@ -0,0 +1,25 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # Bucket Sort
+        buckets = [[] for _ in range(len(nums) + 1)]
+        counts = defaultdict(int)
+
+        for num in nums:
+            counts[num] += 1
+        
+        for num, freq in counts.items():
+            buckets[freq].append(num)
+        
+        res = []
+        for i in range(len(nums), -1, -1):
+            for num in buckets[i]:
+                res.append(num)
+
+                if len(res) == k:
+                    return res
+        
+        
+                
+
+
+        
